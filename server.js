5c0756df443dc0903a5c6b61135871c3cb588539
smctdev <168695959+smctdev@users.noreply.github.com>
2025-04-23 16:23:40 +0800

    updated

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+//app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '/app/uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
